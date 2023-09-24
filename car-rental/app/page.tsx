28fd040712a83b1db9cf07cea1cf1c72954ad066
nandish.ug <nandish.ug@gmail.com>
2023-09-24 13:51:38 +0100

    Search Filter

--- car-rental/app/page.tsx
+++ car-rental/app/page.tsx
@@ -1,10 +1,12 @@
 "use client"
 import Hero from "@/components/Home/Hero";
+import SearchInput from "@/components/Home/SearchInput";
 
 export default function Home() {
   return (
     <div className="p-5 sm:px-10 md:px-20">
       <Hero />
+      <SearchInput />
     </div>    
   )
 }
